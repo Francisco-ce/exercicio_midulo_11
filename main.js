d3aececa84c74dfdb078650120d5901dc222a65e
Francisco Corrêa <franciscopcorrea@gmail.com>
2023-03-02 17:58:08 -0300

    exercicio_modulo_11

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#carrossel-imagens').slick({
+        autoplay:true,
+    });
+
+    $('.menu-hamburguer').click(function() {
+        $('nav').slideToggle();
+    });
+})
